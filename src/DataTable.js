57a19b2cccff16b5178fe23885a2f34f19088fd5
marius <marius.kelpsa@gmail.com>
2015-09-09 11:34:14 +0100

    added travis support; jshitrc; updated gitignore; started working on datepicker controls

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var React = require('react');
 
 var DataTable = React.createClass({
@@ -26,24 +28,26 @@ var DataTable = React.createClass({
 	},
 
 	render: function() {
-		var rows = [],
+		let rows = [],
 			values = [],
-			data = this.props.data,
+		  data = this.props.data,
 			year = this.props.startYear;
 
-		data.forEach(function(item, i) {
+		data.forEach((item, i) => {
 			values.push(<td key={item} onClick={this.props.onClick}>{item}</td>);
 			year++;
 			if (i > 0 && (i + 1) % this.props.columns === 0) {
 				rows.push(<tr key={i}>{values}</tr>);
 				values = [];
 			}
-		}.bind(this));
+		});
 		//last batch of values
 		rows.push(<tr key={data.length}>{values}</tr>);
+
 		return (
 			<table>
 				<tbody>
+					{this.buildControlRow()}
 					{rows}
 				</tbody>
 			</table>
